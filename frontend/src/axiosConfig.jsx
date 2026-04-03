64a79ef0b471e9a26c718a580fc266ebc53a9593
MatthewMyspace <matthew.myspace1@gmail.com>
2026-04-03 19:05:40 +1000

    fix: URL EC2  public

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-   baseURL: 'http://3.27.187.218:5001', // live
+   baseURL: '', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
