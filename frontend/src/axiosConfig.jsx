054266033a5b4efaa4f2a3fc600f146962508c84
MatthewMyspace <matthew.myspace1@gmail.com>
2026-04-16 08:37:05 +1000

    Test before class

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-   baseURL: '', // live
+  baseURL: 'http://localhost:5001', // local
+   //baseURL: '', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
