fd5a2aa9327b8b6f31d44983c0b2afa5eb85453e
MatthewMyspace <matthew.myspace1@gmail.com>
2026-05-21 17:52:05 +1000

    Fix axios baseURL for production

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: '/api', // local
    //baseURL: '', // live
   headers: { 'Content-Type': 'application/json' },
 });
